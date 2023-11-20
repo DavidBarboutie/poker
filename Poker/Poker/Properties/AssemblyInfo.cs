64eb135983ab4a6fdb857e4fcfc94bc8e7aca931
David BARBOUTIE <d.barboutie@cite-scolaire.lan>
2023-11-20 17:32:20 +0100

    test de fichier

--- Poker/Poker/Properties/AssemblyInfo.cs
+++ Poker/Poker/Properties/AssemblyInfo.cs
@@ -9,11 +9,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Poker")]
+[assembly: AssemblyTitle("poker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Poker")]
+[assembly: AssemblyProduct("poker")]
 [assembly: AssemblyCopyright("Copyright 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
