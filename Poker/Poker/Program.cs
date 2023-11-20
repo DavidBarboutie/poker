2dd7916023b8065556cae6c450d64a4b2ed37a61
David BARBOUTIE <d.barboutie@cite-scolaire.lan>
2023-11-20 17:54:56 +0100

    rectification de la syntaxe

--- Poker/Poker/Program.cs
+++ Poker/Poker/Program.cs
@@ -17,7 +17,7 @@ namespace poker
 			//
 			// DECLARATION DES DONNEES
 			//
-			// Importation des DL (librairies de code) permettant de gérer les couleurs en mode console
+			// Importation des DLL (librairies de code) permettant de gérer les couleurs en mode console
 			[DllImport("kernel32.dll")]
 			public static extern bool SetConsoleTextAttribute(IntPtr hConsoleOutput, int wAttributes);
 			[DllImport("kernel32.dll")]
@@ -52,22 +52,40 @@ namespace poker
 			
 			
 			// *** VARIABLES
-			// Coordonnées de départ pour l'affichage public static coordonnees depart;
+			// Coordonnées de départ pour l'affichage
+			public static coordonnees depart;
+			
 			// Fin du jeu
 			public static bool fin = false;
+			
 			// Valeurs des cartes : As, Roi,...
 			public static char [] valeurs = {'A','R','D','V','X','9','8','7'};
-			// Codes ASCII (3 : coeur, 4 : carreau, 5 : trèfle, 6 : pique) public static int [] familles = {3,4,5,6};
+			
+			// Codes ASCII (3 : coeur, 4 : carreau, 5 : trèfle, 6 : pique) 
+			public static int [] familles = {3,4,5,6};
+			
 			// Numéros des cartes à échanger
 			public static int [] echange = {0,0,0,0};
+			
 			// Jeu de 5 cartes
 			public static carte[] MonJeu = new carte[5];
 			
+			// Test de la combinaison
+			switch (cherche_combinaison(unJeu))
+			{
+			case combinaison.RIEN :
+				afficher_message("rien du tout... desole!", couleur.ROUGE, new coordonnees { x = 24, y = 15 });
+				break;
+			case combinaison.PAIRE:
 			
 			Console.ReadKey(true);
 		}
 		// Génère aléatoirement une carte : {valeur;famille}
-		// Retourne une expression de type "carte" private static carte tirage()
+		// Retourne une expression de type "carte"
+		private static carte tirage(){
+		
+		}
+			
 		// Indique si une carte est déjà présente dans le jeu
 		// Paramètres : une carte, le jeu 5 cartes, le numéro de la carte dans le jeu
 		// Retourne un booléen
@@ -84,6 +102,28 @@ namespace poker
 		// La valeur retournée est un élement de l'énumération 'combinaison' private static combinaison chercheCombinaison(carte[] unJeu)
 		// Calcul et affichage du résultat
 		// Paramètre : le tableau de 5 cartes
+		
+		// Choix de la couleur
+		// * couleur.ROUGE pour coeur et carreau
+		// * couleur.NOIRE pour trèfle et pique couleur lacouleur;
+		if (uneCarte.famille == 3 || uneCarte.famille == 4) lacouleur = couleur.ROUGESURBLANC;
+		Affichage de la carte depuis le haut :
+		else
+		lacouleur = couleur.NOIRESURBLANC;
+		// Positionnement et affichage
+		// REMARQUE : dans la fenêtre exécution, choisir "Police Roster" pour afficher les symboles... afficher_message
+		(
+		string.Format("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}\n",'*','-','-','-','-','-','-','-','-','-','*'),
+		lacouleur,
+		new coordonnees {x= colonne * 15, y=1 }
+		);
+		afficher_message (
+		string.Format("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}\n",'|',
+		(char)uneCarte.famille,' ', (char)uneCarte.famille,' ', (char)uneCarte.famille,' ',
+		(char)uneCarte.famille,' ',
+		(char)uneCarte.famille, '|'), lacouleur, new coordonnees { x = colonne * 15, y = 2 }
+		);
+			
 			
 		private static void afficheResultat(carte [] unJeu)
 		// Enregistrer le jeu dans un fichier
