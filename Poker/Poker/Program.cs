da56922ba0320f97d0e97ee5432303fdd20a06de
David BARBOUTIE <d.barboutie@cite-scolaire.lan>
2023-12-07 17:25:55 +0100

    réorganisation du code et ajout de la fonction d'échange de cartes

--- Poker/Poker/Program.cs
+++ Poker/Poker/Program.cs
@@ -11,218 +11,265 @@ using System.IO;
 using System.Text;
 using System.Runtime.InteropServices;
 
-
-
-namespace poker
+namespace Poker
 {
-	class Program
-	{
-		//
-		// DECLARATION DES DONNEES
-		//
-		// Importation des DLL (librairies de code) permettant de gérer les couleurs en mode console
-		
-		[DllImport("kernel32.dll")]
-		public static extern bool SetConsoleTextAttribute(IntPtr hConsoleOutput, int wAttributes);
-		[DllImport("kernel32.dll")]
-		public static extern IntPtr GetStdHandle(uint nStdHandle);
-			
-		static uint STD_OUTPUT_HANDLE = 0xfffffff5;
-		static IntPtr hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-			
-		// Pour utiliser la fonction C 'getchar()' : sasie d'un caractère 
-		[DllImport("msvcrt")]
-		static extern int _getch();
-			
-		// *** TYPES DE DONNEES
-			
-		// Codes COULEUR
-		public enum couleur {VERT=10,ROUGE=12,JAUNE=14,BLANC=15,NOIRE=0,ROUGESURBLANC=252,NOIRESURBLANC= 240};
-			
-		public struct coordonnees
-			{
-				public int x; 
-				public int y;
-			}
-		
-		public struct carte
-			{
-				public char valeur; 
-				public int famille;
-			}
-			
-		public enum combinaison {RIEN,PAIRE,DOUBLE_PAIRE,BRELAN,QUINTE,FULL,COULEUR,CARRE,QUINTE_FLUSH};
-			
-			
-			
-		// *** VARIABLES
-		// Coordonnées de départ pour l'affichage
-		public static coordonnees depart;
-			
-		// Fin du jeu
-		public static bool fin = false;
-		
-		// Valeurs des cartes : As, Roi,...
-		public static char [] valeurs = {'A','R','D','V','X','9','8','7'};
-			
-		// Codes ASCII (3 : coeur, 4 : carreau, 5 : trèfle, 6 : pique) 
-		public static int [] familles = {3,4,5,6};
-			
-		// Numéros des cartes à échanger
-		public static int [] echange = {0,0,0,0};
-			
-		// jeu de 5 cartes
-		public static carte[] unJeu = new carte[5];
-			
-		// Test de la combinaison
-		switch (cherche_combinaison(unJeu))
-		{
-		case combinaison.RIEN :
-			afficher_message("rien du tout... desole!", couleur.ROUGE, new coordonnees { x = 24, y = 15 }); 
-			break;
-			
-			
-			
-		// Génère aléatoirement une carte : {valeur;famille}
-		// Retourne une expression de type "carte"
-		private static carte tirage(){
-			Random rnd = new Random();
+    class Program
+    {
+        // -----------------------
+        // DECLARATION DES DONNEES
+        // -----------------------
+        // Importation des DL (librairies de code) permettant de gérer les couleurs en mode console
+        [DllImport("kernel32.dll")]
+        public static extern bool SetConsoleTextAttribute(IntPtr hConsoleOutput, int wAttributes);
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetStdHandle(uint nStdHandle);
+        static uint STD_OUTPUT_HANDLE = 0xfffffff5;
+        static IntPtr hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        // Pour utiliser la fonction C 'getchar()' : sasie d'un caractère
+        [DllImport("msvcrt")]
+        static extern int _getche();
+
+        //-------------------
+        // TYPES DE DONNEES
+        //-------------------
+
+        // Fin du jeu
+        public static bool fin = false;
+
+        // Codes COULEUR
+        public enum couleur { VERT = 10, ROUGE = 12, JAUNE = 14, BLANC = 15, NOIRE = 0, ROUGESURBLANC = 252, NOIRESURBLANC = 240 };
+
+        // Coordonnées pour l'affichage
+        public struct coordonnees
+        {
+            public int x;
+            public int y;
+        }
+
+        // Une carte
+        public struct carte
+        {
+            public char valeur;
+            public int famille;
+        };
+
+        // Liste des combinaisons possibles
+        public enum combinaison { RIEN, PAIRE, DOUBLE_PAIRE, BRELAN, QUINTE, FULL, COULEUR, CARRE, QUINTE_FLUSH };
+
+        // Valeurs des cartes : As, Roi,...
+        public static char[] valeurs = { 'A', 'R', 'D', 'V', 'X', '9', '8', '7', '6', '5', '4', '3', '2' };
+
+        // Codes ASCII (3 : coeur, 4 : carreau, 5 : trèfle, 6 : pique)
+        public static int[] familles = { 3, 4, 5, 6 };
+
+        // Numéros des cartes à échanger
+        public static int[] echange = { 0, 0, 0, 0 };
+
+        // Jeu de 5 cartes
+        public static carte[] MonJeu = new carte[5];
+
+        //----------
+        // FONCTIONS
+        //----------
+
+        // Génère aléatoirement une carte : {valeur;famille}
+        // Retourne une expression de type "structure carte"
+        public static carte tirage()
+        {
+            Random rnd = new Random();
 			new carte = {valeur = valeurs[rnd], famille = familles[rnd]};
 			return carte;
-		}
-			
-		
-		
-		// Indique si une carte est déjà présente dans le jeu
-		// Paramètres : une carte, le jeu 5 cartes, le numéro de la carte dans le jeu
-		// Retourne un booléen
-		private static bool carteUnique(carte uneCarte, carte[] unJeu, int numero)
-		{
+        }
+
+        // Indique si une carte est déjà présente dans le jeu
+        // Paramètres : une carte, le jeu 5 cartes, le numéro de la carte dans le jeu
+        // Retourne un entier (booléen)
+        public static bool carteUnique(carte uneCarte, carte[] unJeu, int numero)
+        {
 			foreach(carte element in unJeu){
 				if (element.valeur == uneCarte.valeur && element.famille == uneCarte.famille) {
 					return true;
 				}
 			}
 			return false;
-		}
-		
-		// Affiche à l'écran une carte {valeur;famille}
-		private static void affichageCarte(carte uneCarte){
+        }
+
+        // Calcule et retourne la COMBINAISON (paire, double-paire... , quinte-flush)
+        // pour un jeu complet de 5 cartes.
+        // La valeur retournée est un élement de l'énumération 'combinaison' (=constante)
+        public static combinaison chercheCombinaison(ref carte[] unJeu)
+        {
 			
 			
-			
-			// Choix de la couleur
-			// * couleur.ROUGE pour coeur et carreau
-			// * couleur.NOIRE pour trèfle et pique
-			/////int colonne = Console.GetCursorPosition();
-			couleur lacouleur;
-			if (uneCarte.famille == 3 || uneCarte.famille == 4){
-				lacouleur = couleur.ROUGESURBLANC;}
-			else
-			{
-				lacouleur = couleur.NOIRESURBLANC;
-				// Positionnement et affichage
-				// REMARQUE : dans la fenêtre exécution, choisir "Police Roster" pour afficher les symboles...
-				Console.WriteLine
-				(string.Format("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}\n",'*','-','-','-','-','-','-','-','-','-','*'), lacouleur, new coordonnees {x= 1 * 15, y=1 });
-				Console.WriteLine (
-				string.Format("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}\n",'|',
-				(char)uneCarte.famille,' ', (char)uneCarte.famille,' ', (char)uneCarte.famille,' ',
-				(char)uneCarte.famille,' ',
-				(char)uneCarte.famille, '|'), lacouleur, new coordonnees { x = 1 * 15, y = 2 }
-				);
-			}
-		}
-		
-		
-		// Tirage d'un jeu de 5 cartes
-		// Paramètre : le tableau de 5 cartes à remplir
-		private static void tirageDuJeu(carte[] unJeu){
-		for (int i = 0; i < 5; i++) {
+			//cas ou rien n'est présent
+			return combinaison.RIEN;
+        }
+
+        // Echange des cartes
+        // Paramètres : le tableau de 5 cartes et le tableau des numéros des cartes à échanger
+        private static void echangeCarte(carte[] unJeu, int[] e)
+        {
+			carte temp = unJeu[e[0]];
+			unJeu[e[0]] = unJeu[e[1]];
+			unJeu[e[1]] = temp;
+        }
+
+        // Pour afficher le Menu pricipale
+        private static void afficheMenu()
+        {
+			//a faire
+        }
+
+        // Jouer au Poker
+		// Ici que vous appellez toutes les fonction permettant de joueur au poker
+        private static void jouerAuPoker()
+        {
+			//a faire
+        }
+
+        // Tirage d'un jeu de 5 cartes
+        // Paramètre : le tableau de 5 cartes à remplir
+        private static void tirageDuJeu(carte[] unJeu)
+        {
+			for (int i = 0; i < 5; i++) {
 				unJeu[i] = tirage();
 		}
-		
-		
-		
-		// Echange des cartes
-		// Paramètres : le tableau de 5 cartes et le tableau des numéros des cartes à échanger
-		private static void echangeDeCartes(carte[] unJeu, int[] e){
-			//
-		}
-		
-		
-		
-		// Calcule et retourne la combinaison (paire, double-paire…) pour un jeu complet de 5 cartes
-		// La valeur retournée est un élement de l'énumération 'combinaison'
-		private static combinaison chercheCombinaison(carte[] unJeu){
-			
-			int cpt = 0;
-			int paire = 0;
-			int triple = 0;
-			
-			for (int i = 0; i < 5; i++) {
-				carte carte = unJeu[i];
-				for (int j = 0; j < end; i++) {
-					if (carte != unJeu[j]) {
-						if (carte == unJeu[j]){
-							cpt+=1
-						}
-					}
-				}
-				if (cpt == 2){
-					paire += 1;	
-				}
-				else{
-					if (cpt == 3) {
-						triple += 1;
-					}
-					else{
-						if (cpt == 4) {
-							//cas du carré
-							return combinaison.CARRE
-						}
-					}
-				}
-			}
-			if (paire >= 1) {
-				//cas d'une seule paire
-				if (paire == 2) {
-					return combinaison.DOUBLE_PAIRE;
-				}
-				else{
-					if (triple == 0) {
-						return combinaison.PAIRE;
-					}
-					else{
-						return combinaison.BRELAN;
-					}
-				}
-					
-				}
-			}
-		//cas ou rien n'est présent
-		return combinaison.RIEN;
-		}
-		
-		// Calcul et affichage du résultat
-		// Paramètre : le tableau de 5 cartes
-		private static void afficheResultat(carte [] unJeu){}
-		
-		
-		
-		// Enregistrer le jeu dans un fichier
-		private static void enregistrerJeu(carte [] unJeu){}
-		
-		
-		// Voir les scores depuis le fichier 
-		private static void voirScores(){}
-		
-		
-		//jouer au poker
-		public static void jouerAuPoker(carte [] unJeu){}
-		
-		
-		// Affiche le menu du jeu et retourne l'option choisie 
-		public static char afficherMenu(){}
-	}
+        }
+
+        // Affiche à l'écran une carte {valeur;famille} 
+        private static void affichageCarte()
+        {
+            //----------------------------
+            // TIRAGE D'UN JEU DE 5 CARTES
+            //----------------------------
+            int left = 0;
+            int c = 1;
+            // Tirage aléatoire de 5 cartes
+            for (int i = 0; i < 5; i++)
+            {
+                // Tirage de la carte n°i (le jeu doit être sans doublons !)
+
+                // Affichage de la carte
+                if (MonJeu[i].famille == 3 || MonJeu[i].famille == 4)
+                    SetConsoleTextAttribute(hConsole, 252);
+                else
+                    SetConsoleTextAttribute(hConsole, 240);
+                Console.SetCursorPosition(left, 5);
+                Console.Write("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}\n", '*', '-', '-', '-', '-', '-', '-', '-', '-', '-', '*');
+                Console.SetCursorPosition(left, 6);
+                Console.Write("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}\n", '|', (char)MonJeu[i].famille, ' ', (char)MonJeu[i].famille, ' ', (char)MonJeu[i].famille, ' ', (char)MonJeu[i].famille, ' ', (char)MonJeu[i].famille, '|');
+                Console.SetCursorPosition(left, 7);
+                Console.Write("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}\n", '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|');
+                Console.SetCursorPosition(left, 8);
+                Console.Write("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}\n", '|', (char)MonJeu[i].famille, ' ', ' ', ' ', ' ', ' ', ' ', ' ', (char)MonJeu[i].famille, '|');
+                Console.SetCursorPosition(left, 9);
+                Console.Write("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}\n", '|', ' ', ' ', ' ', (char)MonJeu[i].valeur, (char)MonJeu[i].valeur, (char)MonJeu[i].valeur, ' ', ' ', ' ', '|');
+                Console.SetCursorPosition(left, 10);
+                Console.Write("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}\n", '|', (char)MonJeu[i].famille, ' ', ' ', (char)MonJeu[i].valeur, (char)MonJeu[i].valeur, (char)MonJeu[i].valeur, ' ', ' ', (char)MonJeu[i].famille, '|');
+                Console.SetCursorPosition(left, 11);
+                Console.Write("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}\n", '|', ' ', ' ', ' ', (char)MonJeu[i].valeur, (char)MonJeu[i].valeur, (char)MonJeu[i].valeur, ' ', ' ', ' ', '|');
+                Console.SetCursorPosition(left, 12);
+                Console.Write("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}\n", '|', (char)MonJeu[i].famille, ' ', ' ', ' ', ' ', ' ', ' ', ' ', (char)MonJeu[i].famille, '|');
+                Console.SetCursorPosition(left, 13);
+                Console.Write("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}\n", '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|');
+                Console.SetCursorPosition(left, 14);
+                Console.Write("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}\n", '|', (char)MonJeu[i].famille, ' ', (char)MonJeu[i].famille, ' ', (char)MonJeu[i].famille, ' ', (char)MonJeu[i].famille, ' ', (char)MonJeu[i].famille, '|');
+                Console.SetCursorPosition(left, 15);
+                Console.Write("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}\n", '*', '-', '-', '-', '-', '-', '-', '-', '-', '-', '*');
+                Console.SetCursorPosition(left, 16);
+                SetConsoleTextAttribute(hConsole, 10);
+                Console.Write("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}\n", ' ', ' ', ' ', ' ', ' ', c, ' ', ' ', ' ', ' ', ' ');
+                left = left + 15;
+                c++;
+            }
+
+        }
+
+        // Enregistre le score dans le txt
+        private static void enregistrerJeu(carte[] unJeu)
+        {
+          //a faire
+        }
+
+        // Affiche le Scores
+        private static void voirScores()
+        {
+           //a faire
+        }
+
+        // Affiche résultat
+        private static void afficheResultat(carte[] unJeu)
+        {
+            SetConsoleTextAttribute(hConsole, 012);
+            Console.Write("RESULTAT - Vous avez : ");
+            try
+            {
+                // Test de la combinaison
+                switch (chercheCombinaison(ref MonJeu))
+                {
+                    case combinaison.RIEN:
+                        Console.WriteLine("rien du tout... desole!"); break;
+                    case combinaison.PAIRE:
+                        Console.WriteLine("une simple paire..."); break;
+                    case combinaison.DOUBLE_PAIRE:
+                        Console.WriteLine("une double paire; on peut esperer..."); break;
+                    case combinaison.BRELAN:
+                        Console.WriteLine("un brelan; pas mal..."); break;
+                    case combinaison.QUINTE:
+                        Console.WriteLine("une quinte; bien!"); break;
+                    case combinaison.FULL:
+                        Console.WriteLine("un full; ouahh!"); break;
+                    case combinaison.COULEUR:
+                        Console.WriteLine("une couleur; bravo!"); break;
+                    case combinaison.CARRE:
+                        Console.WriteLine("un carre; champion!"); break;
+                    case combinaison.QUINTE_FLUSH:
+                        Console.WriteLine("une quinte-flush; royal!"); break;
+                };
+            }
+            catch { }
+        }
+
+
+        //--------------------
+        // Fonction PRINCIPALE
+        //--------------------
+        static void Main(string[] args)
+        {
+            //---------------
+            // BOUCLE DU JEU
+            //---------------
+            char reponse;
+            while (true)
+            {
+                afficheMenu();
+                reponse = (char)_getche();
+                if (reponse != '1' && reponse != '2' && reponse != '3')
+                {
+                    Console.Clear();
+                    afficheMenu();
+                }
+                else
+                {
+                SetConsoleTextAttribute(hConsole, 015);
+                // Jouer au Poker
+                if (reponse == '1')
+                {
+                    int i = 0;
+                    jouerAuPoker();
+                }
+
+                if (reponse == '2')
+                    voirScores();
+
+                if (reponse == '3')
+                    break;
+            }
+            }
+            Console.Clear();
+        }
+    }
 }
+
+
+
